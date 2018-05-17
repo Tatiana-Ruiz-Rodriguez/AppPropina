63faf53b72d4960dc44cd7c832660a0792d300d0
Tatiana-Ruiz-Rodriguez <tatianaruiz@compucentroltda.com>
2018-05-16 20:51:17 -0500

    ViewModel

--- AppPropina/AppPropina/ViewModel/PropinaViewModel.cs
+++ AppPropina/AppPropina/ViewModel/PropinaViewModel.cs
@@ -6,11 +6,14 @@ namespace AppPropina.ViewModel
 {
 
     using Model;
+    using Xamarin.Forms;
+
     class PropinaViewModel
     {
         #region Propiedades
 
         public Propina PropinaModel { get; set; }
+        public Command CalcularCommand { get; set; }
 
         #endregion
 
@@ -19,6 +22,21 @@ namespace AppPropina.ViewModel
         public PropinaViewModel()
         {
             PropinaModel = new Propina();
+            CalcularCommand = new Command(Calcular);
+        }
+
+        private void Calcular(object obj) {
+
+            PropinaModel.TotalCuenta = decimal.Parse(entTotal.Text);
+            PropinaModel.LaPropina = int.Parse(entPropina.Text);
+            PropinaModel.NumeroPersonas = int.Parse(entPersonas.Text);
+
+            var totalPropina = (total * (propina / 100)).ToString();
+
+            txtPropina.Detail = totalPropina.ToString();
+            txtPersona.Detail = (total / personas).ToString();
+            txtTotal.Detail = (total + totalPropina).ToString();
+
         }
 
         #endregion
