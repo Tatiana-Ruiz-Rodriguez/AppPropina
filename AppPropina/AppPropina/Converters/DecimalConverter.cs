f230e3533c837053cecc720e4e6a13be07ba0793
Tatiana-Ruiz-Rodriguez <Aprendiz Sena@TUNDFPCGE7F369.FormacionSena.RED>
2018-05-22 21:26:29 -0500

    Resultado

---
+++ AppPropina/AppPropina/Converters/DecimalConverter.cs
@@ -0,0 +1,24 @@
+﻿
+namespace AppPropina.Converters
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Text;
+    using Xamarin.Forms;
+
+    class DecimalConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var valor = (decimal)value;
+            return valor.ToString("C");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string valor = (string)value;
+            return valor.Remove(0);
+        }
+    }
+}
